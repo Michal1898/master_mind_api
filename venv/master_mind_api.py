76e24b8e9e2836b17d13a14bd085449918cd2a0a
Michal1898 <michal.volf@gmail.com>
2021-06-27 05:04:31 +0200

    uuid added

--- venv/master_mind_api.py
+++ venv/master_mind_api.py
@@ -1,32 +1,27 @@
 from flask import Flask, jsonify, request
-
+import uuid
 
 import datetime
-from random import choice, randrange, getrandbits
+from random import choice, randrange, getrandbits, shuffle
 from datetime import timedelta, date
-from random import shuffle
-from dateutil.relativedelta import relativedelta
-import os
-from radar import random_datetime
-
-FEMALE = True
-MALE = False
 
 app = Flask(__name__)
 
 @app.route("/")
 def dummy_api():
     person = {}
-    age_max=99
-    age_min=1
-    gender=MALE
 
-    count=int(request.args.get("att"))
-    gender2=str(request.args.get("uuid"))
-    gender2=gender2.upper()
-    if gender2=="FEMALE":
-        gender=FEMALE
 
+    #code = str(request.args.get("code"))
+    code="new_game"
+    code= code.lower()
+    if code=="new_game":
+        game_uuid = uuid.uuid4()
+        game_uuid = str(game_uuid)
+        game_uuid = game_uuid[:8]
+        return game_uuid
+    else:
+        return "ahoj michale."
 
     return jsonify(human_list)
 
